0b1d1d428312b93a2d35c747a906294f401d994f
js-bruno <brunocebrsilva@gmail.com>
2025-11-11 07:36:09 -0300

    fix: adding some services

--- configuration.nix
+++ configuration.nix
@@ -21,163 +21,195 @@
   virtualisation = {
     libvirtd.enable = true;
     spiceUSBRedirection.enable = true;
-  };
-
-  services = {
-    printing.enable = true;
-    pulseaudio.enable = false;
-    flatpak.enable = true;
-
-    jellyfin = {
-      enable = true;
-      openFirewall = true;
-    };
-
-    pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-      wireplumber.enable = true;
-    };
-
-    xserver = {
-      enable = true;
-      xkb = {
-        layout = "us";
-        variant = "";
-      };
-      displayManager = {
-        defaultSession = "xterm+i3";
-        gdm.enable = true;
-      };
-      desktopManager = {
-        gnome.enable = true;
-        xterm.enable = true;
-      };
-      windowManager.i3 = {
+    docker = {
+      enable = false;
+      rootless = {
         enable = true;
-        extraPackages = with pkgs; [
-            rofi
-            feh
-            i3status 
-            i3lock 
-            picom
-        ];
+        setSocketVariable = true;
+        daemon.settings = {
+          dns = [ "1.1.1.1" "8.8.8.8" ];
+          registry-mirrors = [ "https://mirror.gcr.io" ];
+        };
       };
     };
   };
 
-  users = {
-    defaultUserShell=pkgs.zsh; 
-    users= {
-      lacon = {
-        isNormalUser = true;
-        description = " jose bruno";
-        extraGroups = [ "networkmanager" "wheel" "libvirtd"];
-        shell=pkgs.zsh;
+    services = {
+      printing.enable = true;
+      pulseaudio.enable = false;
+      flatpak.enable = true;
+
+      jellyfin = {
+        enable = true;
+        openFirewall = true;
+      };
+
+      pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+        wireplumber.enable = true;
+      };
+
+      xserver = {
+        enable = true;
+        xkb = {
+          layout = "us";
+          variant = "";
+        };
+        displayManager = {
+          defaultSession = "gnome-xorg";
+          gdm.enable = true;
+        };
+        desktopManager = {
+          gnome.enable = true;
+          xterm.enable = true;
+        };
+        windowManager.i3 = {
+          enable = true;
+          extraPackages = with pkgs; [
+              rofi
+              feh
+              i3status 
+              i3lock 
+              picom
+              polybar
+          ];
+        };
       };
     };
-  };
 
-  nixpkgs.config.allowUnfree = true;
-
-  environment = {
-    shellAliases = {
-      ll = "ls -l";
+    users = {
+      defaultUserShell=pkgs.zsh; 
+      users= {
+        lacon = {
+          isNormalUser = true;
+          description = " jose bruno";
+          extraGroups = [ "docker" "networkmanager" "wheel" "libvirtd"];
+          shell=pkgs.zsh;
+        };
+      };
     };
 
-    systemPackages = with pkgs; [
-        nixd
-        hyperfine
-        gopls
+    nixpkgs.config.allowUnfree = true;
 
-        hugo
-        nodejs_24
+    environment = {
+      shellAliases = {
+        ll = "ls -l";
+      };
 
-        qemu
-        libgcc
-        ripgrep
-        zsh-autosuggestions
-        git
-        dysk
-        xclip
-        vivaldi
-        librewolf
-        curl
-        stow
-        zoxide
-        obsidian
-        qbittorrent
-        vlc
-        plex
-        jellyfin
-        jellyfin-web
-        jellyfin-ffmpeg
-        visidata
+      systemPackages = with pkgs; [
+          flameshot
+          xclip
+          pavucontrol
+          nixd
+          hyperfine
+          gopls
 
-        alacritty
-        ghostty
-        wezterm
-        kitty
-        tmux
+          vscode
+          docker-compose
+          lazydocker
+          discord-ptb
+          hugo
+          sqlite
+          nodejs_24
+          typescript-language-server
+          vue-language-server
 
-        mangohud 
-        prismlauncher
-        lutris
-        protonup-qt 
-        bottles 
-        gearlever
+          ungoogled-chromium
+          qemu
+          libgcc
+          ripgrep
+          zsh-autosuggestions
+          git
+          dysk
+          xclip
+          librewolf
+          curl
+          stow
+          zoxide
+          obsidian
+          qbittorrent
+          vlc
+          stremio
+          plex
+          jellyfin
+          jellyfin-web
+          jellyfin-ffmpeg
+          visidata
 
-        btop
-        fortune
-        lazygit
+          alacritty
+          ghostty
+          wezterm
+          kitty
+          tmux
+          kando
+          fzf
 
-        go
-        vim
-        neovim
-        lua-language-server
+          sqlitebrowser
+          prismlauncher
+          lutris
+          wine
+          wine64
+          winetricks
+          vulkan-tools
+          protonup-qt 
+          bottles 
+          gearlever
 
-        ];
-  };
+          btop
+          fortune
+          lazygit
 
-  fonts.packages = with pkgs; [
-    nerd-fonts.go-mono
-      nerd-fonts.iosevka-term
-      nerd-fonts.daddy-time-mono
-      nerd-fonts.envy-code-r
-      nerd-fonts.comic-shanns-mono
-      nerd-fonts.shure-tech-mono
-  ];
+          go
+          vim
+          neovim
+          lua-language-server
 
-  boot = {
-    loader.systemd-boot.enable = true;
-    loader.efi.canTouchEfiVariables = true;
-  };
-
-  security.rtkit.enable = true;
-  networking = {
-    hostName = "nixos"; 
-    networkmanager.enable = true;
-    interfaces.eth0.ipv4.addresses = [ { address = "192.168.1.99"; prefixLength = 24; } ];
-    defaultGateway = "192.168.1.1";
-    nameservers = [ "8.8.8.8" ];
-    useDHCP = false;
-  };
-  time.timeZone = "America/Fortaleza";
-  i18n = {
-    defaultLocale = "pt_BR.UTF-8";
-    extraLocaleSettings = {
-      LC_ADDRESS = "pt_BR.UTF-8";
-      LC_IDENTIFICATION = "pt_BR.UTF-8";
-      LC_MEASUREMENT = "pt_BR.UTF-8";
-      LC_MONETARY = "pt_BR.UTF-8";
-      LC_NAME = "pt_BR.UTF-8";
-      LC_NUMERIC = "pt_BR.UTF-8";
-      LC_PAPER = "pt_BR.UTF-8";
-      LC_TELEPHONE = "pt_BR.UTF-8";
-      LC_TIME = "pt_BR.UTF-8";
+          ];
     };
-  };
-  system.stateVersion = "25.05";
-}
+
+    fonts.packages = with pkgs; [
+      nerd-fonts.go-mono
+        nerd-fonts.iosevka-term
+        nerd-fonts.daddy-time-mono
+        nerd-fonts.envy-code-r
+        nerd-fonts.comic-shanns-mono
+        nerd-fonts.shure-tech-mono
+    ];
+
+    boot = {
+      loader.systemd-boot.enable = true;
+      loader.efi.canTouchEfiVariables = true;
+    };
+
+    security.rtkit.enable = true;
+    networking = {
+      hostName = "nixos"; 
+      networkmanager.enable = true;
+      interfaces.eth0.ipv4.addresses = [ { address = "192.168.1.99"; prefixLength = 24; } ];
+      defaultGateway = "192.168.1.1";
+      nameservers = [ "1.1.1.1" "8.8.8.8" ];
+      useDHCP = false;
+      hosts = {
+        "185.199.110.133" = ["raw.githubusercontent.com"];
+      };
+    };
+    time.timeZone = "America/Fortaleza";
+    i18n = {
+      defaultLocale = "pt_BR.UTF-8";
+      extraLocaleSettings = {
+        LC_ADDRESS = "pt_BR.UTF-8";
+        LC_IDENTIFICATION = "pt_BR.UTF-8";
+        LC_MEASUREMENT = "pt_BR.UTF-8";
+        LC_MONETARY = "pt_BR.UTF-8";
+        LC_NAME = "pt_BR.UTF-8";
+        LC_NUMERIC = "pt_BR.UTF-8";
+        LC_PAPER = "pt_BR.UTF-8";
+        LC_TELEPHONE = "pt_BR.UTF-8";
+        LC_TIME = "pt_BR.UTF-8";
+      };
+    };
+    system.stateVersion = "25.05";
+  }
--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -45,6 +45,9 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.opengl.driSupport32Bit = true;
+  hardware.opengl.enable = true;
+  hardware.pulseaudio.support32Bit = true;
   hardware.nvidia = {
     modesetting.enable = true;
 
