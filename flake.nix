703c3e1f7c8f95b442304aa112e306bede5fbf89
js-bruno <brunocebrsilva@gmail.com>
2026-08-16 00:37:55 -0300

    fix: remove unsed microvm

--- flake.nix
+++ flake.nix
@@ -2,11 +2,9 @@
   description = "My declartive nix-minecraft server with my os configuration";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    microvm.url = "github:microvm-nix/microvm.nix";
-    microvm.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = {self, nixpkgs, microvm, ... }@inputs:
+  outputs = {self, nixpkgs, ... }@inputs:
   {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
@@ -14,7 +12,6 @@
 
       modules = [
         ./configuration.nix
-        ./microvm-host.nix
       ];
     };
   };
